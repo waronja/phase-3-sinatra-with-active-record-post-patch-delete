8ee44247e62c44a78dc6ad265419703c1c8a3eb0
simon waronja <simon.waronja@student.moringaschool.com>
2022-09-08 07:51:04 +0300

    Done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
 
@@ -14,6 +15,37 @@ class ApplicationController < Sinatra::Base
         user: { only: [:name] }
       } }
     })
+  end
+
+
+  delete '/reviews/:id' do
+    #fiind the review using the ID
+    review = Review.find(params[:id])
+    #delete the review
+    review.destroy
+    #send a response with the deleted review as JSON
+    review.to_json
+  end
+
+
+  post '/reviews' do 
+    review = review.create(
+      score: params[:score],
+      comment: params[:comment],
+      game_id: params[:game_id],
+      user_id: params[:user_id]
+    )
+    review.to_json  
+
+  end
+
+  patch '/reviews/:id' do
+    review = Review.find(params[:id])
+    review.update(
+      score: params[:score],
+      comment: params[:comment]
+    )
+    review.to_json
   end
 
 end
